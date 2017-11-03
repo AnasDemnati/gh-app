086aecf31e9d76cfc1c2aaef3dba4d522280e6ad
anas <anasblade555@gmail.com>
2017-11-03 15:07:37 +0000

    sample app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ghAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ghAppApp
+ */
+angular.module('ghAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
